8fa98eedf3c1a25d2b9e09a307f5b9d41bca8e50
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-04-07 16:26:18 -0700

    adds tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener("submit", e => {e.preventDefault()
+  handleToDo(e.target.new_task_description.value)})
+
+})
+
+
+function handleToDo(todo){
+    let p = document.createElement("p")
+    let btn = document.createElement("button")
+    btn.addEventListener("click", addingEventListenerToButton)
+    p.textContent = `${todo} `
+    btn.textContent = "x"
+    document.getElementById("tasks").appendChild(p)
+    p.appendChild(btn)
+    
+}
+
+function addingEventListenerToButton(e){
+  e.target.parentNode.remove()
+}
